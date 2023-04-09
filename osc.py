e6f4f7325ee9b9dc11203f09fe334013e8b422d6
Robson França <robson@morot.com.br>
2023-04-09 13:31:07 -0300

    Color computer color scheme for the oscilloscope

--- osc.py
+++ osc.py
@@ -10,15 +10,17 @@ def render(frame_index : int, channels : list) -> im.Image:
     CHAN_HEIGHT = SIZE[1] * 1.0 / NUM_CHANNELS
     CHAN_LIM = CHAN_HEIGHT * 0.8
     FACTOR = (CHAN_LIM / 2.0) / 32.0
-    frame=im.new("RGB",SIZE)
+    frame=im.new("RGB",SIZE,(0,255,0))
     draw = dr.Draw(frame)
     
     pos = frame_index * 204
     for i, channel in enumerate(channels):
         my = i * CHAN_HEIGHT + (CHAN_LIM / 2.0)
+        if i > 0:
+            draw.line(xy=(0, i * CHAN_HEIGHT, SIZE[0], i * CHAN_HEIGHT), fill=(33,160,33))
         coords = list(zip((x for x in range(SIZE[0])),(int((32.0 - v) * FACTOR + my) for v in channel[pos:pos+SIZE[0]])))
         for n in range(1,len(coords)):
-            draw.line(xy=(coords[n-1][0],coords[n-1][1],coords[n][0],coords[n][1]), fill=(255,255,255))
+            draw.line(xy=(coords[n-1][0],coords[n-1][1],coords[n][0],coords[n][1]), fill=(0,68,0))
     return frame
 
 def process(channels : list, filename : str):
